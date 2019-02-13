f9cd7eeac8dc42e186d54bbf60a3f0a066e59347
Laureline Martin <laureline.martin@ens.uvsq.fr>
2019-02-13 10:52:06 +0100

    Creation de la classe personne avec la classe Builder

---
+++ S2/ProgGL/TD2/Annuaire/src/Annuaire/Personne.java
@@ -0,0 +1,23 @@
+package Annuaire;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public final class Personne {
+	private String Nom;
+	private String Prenom;
+	private String Fonction;
+	private java.time.LocalDate dateNaissance;
+	private  ArrayList<?> Numero;
+	
+	public static class Builder{
+		private  ArrayList<?> Numero;
+	}
+	
+	public Personne(Builder builder) {
+		
+	}
+	
+	
+
+}
