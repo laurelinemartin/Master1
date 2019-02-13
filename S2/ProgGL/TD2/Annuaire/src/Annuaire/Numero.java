9b970a52c04c5f1efb88ba908c389463bb251dc9
Laureline Martin <laureline.martin@ens.uvsq.fr>
2019-02-13 11:02:15 +0100

    Ajout de la classe Numero

---
+++ S2/ProgGL/TD2/Annuaire/src/Annuaire/Numero.java
@@ -0,0 +1,16 @@
+package Annuaire;
+
+public class Numero {
+	private int Numero;
+	
+	public Numero() {}
+
+	public int getNumero() {
+		return Numero;
+	}
+
+	public void setNumero(int numero) {
+		Numero = numero;
+	}
+
+}
--- S2/ProgGL/TD2/Annuaire/src/Annuaire/Personne.java
+++ S2/ProgGL/TD2/Annuaire/src/Annuaire/Personne.java
@@ -8,14 +8,38 @@ public final class Personne {
 	private String Prenom;
 	private String Fonction;
 	private java.time.LocalDate dateNaissance;
-	private  ArrayList<?> Numero;
+	private Numero Numero;
 	
 	public static class Builder{
-		private  ArrayList<?> Numero;
+		private String Nom;
+		private String Prenom;
+		private String Fonction;
+		private java.time.LocalDate dateNaissance;
+		
+		private Numero Numero = null;	
+		
+		public Builder(String Nom, String Prenom, String Fonction, java.time.LocalDate dateNaissance) {
+			this.Nom = Nom;
+			this.Prenom = Prenom;
+			this.Fonction = Fonction;
+			this.dateNaissance = dateNaissance;
+		}
+		
+		public Builder Numero(Numero Numero) {
+			this.Numero = Numero;
+			return this	;
+		}
+		
+		public Personne build() {
+			return new Personne(this);
+		}
 	}
 	
 	public Personne(Builder builder) {
-		
+		this.Nom = builder.Nom;
+		this.Prenom = builder.Prenom;
+		this.dateNaissance = builder.dateNaissance;
+		this.Numero = builder.Numero;
 	}
 	
 	
