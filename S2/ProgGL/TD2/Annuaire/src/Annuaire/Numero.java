1e4ba29112ba7b5c53b76eafcd32dc0c4f1019d2
Laureline Martin <laureline.martin@ens.uvsq.fr>
2019-02-13 11:53:00 +0100

    Rectfications problèmes Numero

--- S2/ProgGL/TD2/Annuaire/src/Annuaire/Numero.java
+++ S2/ProgGL/TD2/Annuaire/src/Annuaire/Numero.java
@@ -1,20 +1,30 @@
 package Annuaire;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Numero {
 	private int Numero;
+	private ArrayList<Integer> listNumeros;
+	private int taille;
 	
-	public Numero() {}
+	public Numero() {
+		this.taille = 0;
+		listNumeros = new ArrayList<>();
+	}
 
 	public int getNumero() {
-		return Numero;
-	}
-
+		return Numero;}
 	public void setNumero(int numero) {
-		Numero = numero;
-	}
-	
-	public Numero add(Numero Num) {
-		this.setNumero(Num.getNumero());
+		Numero = numero;}
+	public int getTaille() {
+		return taille;}
+	public void setTaille(int taille) {
+		this.taille = taille;}
+
+	public Numero add(String type, int Num) {
+		this.listNumeros.add(Numero);
+		this.setTaille(this.getTaille()+1);
 		return this;
 	}
 
